839a0c39856a68be30447b645a25062d127e45b5
JJLEE-20194099 <long.lt194099@gmail.com>
2022-12-16 00:41:37 +0700

    lambda for button

--- dropdown.py
+++ dropdown.py
@@ -3,9 +3,6 @@ from tkinter import *
 root = Tk()
 root.geometry("200x200")
 
-def show():
-    label.config(text = clicked.get())
-
 options = [
     "Monday",
     "Tuesday",
@@ -21,7 +18,7 @@ drop = OptionMenu(root, clicked, *options)
 drop.config(width=20, anchor='w', padx=10, pady=20, highlightthickness=0)
 drop.pack()
 
-button = Button(master=root, text="click me", command=show)
+button = Button(master=root, text="click me", command=lambda: label.config(text = clicked.get()))
 button.pack()
 
 label = Label(root, text = "")
